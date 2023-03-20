a901f3aea8c200e1154f48fd224c6d31e97f0779
Debashish Kundu <debashish.joy@gmail.com>
2023-03-20 12:04:56 +0530

    socket.io server setup

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  console.log("Connected to Socket.IO! \n")
+
+socket.on('join', function(accessToken, store, latitude, longitude) {
+
+    data = {
+        "latitude": parseFloat(latitude),
+        "longitude": parseFloat(longitude)
+    };
+
+    fetch(`http://192.168.137.67:8080/api/v1/join/${store}`, {
+        
+    method: "POST", // or 'PUT'
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${accessToken}`
+    },
+    
+    body: JSON.stringify(data),
+    })
+    .then((response) => response.json())
+    .then((res) => {
+        console.log("Success:", res);
+        io.emit("queue", res)
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+        
+    })
+
+
+  socket.on('leave', function(accessToken, store) {
+
+    fetch(`http://192.168.137.67:8080/api/v1/leave/${store}`, {
+    method: "POST", // or 'PUT'
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${accessToken}`
+    },
+
+    })
+    .then((response) => response.json())
+    .then((res) => {
+        console.log("Success:", res);
+        io.emit("queue", res)
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+})
+});
+
+
+httpServer.listen(3000);
